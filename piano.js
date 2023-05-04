848d6a760cc980f4036599917882dca40cd5833b
Smeu Dragos <smeu_dragos2000@yahoo.com>
2023-05-04 14:33:01 +0300

    first commit

---
+++ piano.js
@@ -0,0 +1,20 @@
+
+const keyPlay = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    if(!audio) return;
+    audio.currentTime = 0; //rewind audio to the start 
+    audio.play();
+    key.classList.add("play")
+}
+
+const keyStop = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    if(!audio) return;
+
+    key.classList.remove("play")
+}
+
+window.addEventListener("keydown", keyPlay)
+window.addEventListener("keyup", keyStop)
