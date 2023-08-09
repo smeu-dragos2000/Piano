83a7b3c19e93e5be12bc9e0fc07503db72badc49
Smeu Dragos <smeu_dragos2000@yahoo.com>
2023-08-09 09:33:24 +0300

    add loading element

--- piano.js
+++ piano.js
@@ -1,20 +1,25 @@
 
+
 const keyPlay = (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
-    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
     if(!audio) return;
     audio.currentTime = 0; //rewind audio to the start 
     audio.play();
-    key.classList.add("play")
+    key.classList.add("play");
 }
 
 const keyStop = (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
-    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
     if(!audio) return;
 
-    key.classList.remove("play")
+    key.classList.remove("play");
 }
 
-window.addEventListener("keydown", keyPlay)
-window.addEventListener("keyup", keyStop)
+window.addEventListener("keydown", keyPlay);
+window.addEventListener("keyup", keyStop);
+
+
+let loader = document.getElementById("preloader");
+window.addEventListener("load", () => loader.style.display = "none")
